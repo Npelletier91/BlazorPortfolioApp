cbd0bca77011eb4798782f73e0e1867efb9906fd
Npelletier91 <nicolas.pelletier@yahoo.com>
2024-04-01 12:49:17 -0400

    First commit of site with basic flashcard functionalities

---
+++ BlazorPortfolio/BlazorPorfolio.Models/Flashcard.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorPorfolio.Models
+{
+    public class Flashcard
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Front text is required")]
+
+        public string FrontText { get; set; }
+        [Required(ErrorMessage = "Back text is required")]
+
+        public string BackText { get; set; }
+        public int UserID { get; set; }
+    }
+}
---
+++ BlazorPortfolio/BlazorPortfolio.Client/Program.cs
@@ -0,0 +1,14 @@
+using Blazored.LocalStorage;
+using BlazorPortfolio.Client;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+builder.Services.AddAuthorizationCore();
+builder.Services.AddCascadingAuthenticationState();
+builder.Services.AddSingleton<AuthenticationStateProvider, PersistentAuthenticationStateProvider>();
+
+builder.Services.AddBlazoredLocalStorage();
+
+await builder.Build().RunAsync();

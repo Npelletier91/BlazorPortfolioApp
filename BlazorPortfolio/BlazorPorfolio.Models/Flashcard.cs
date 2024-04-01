8daa68c012ae737288944de420223abb2f3a5a50
Npelletier91 <nicolas.pelletier@yahoo.com>
2024-04-01 16:14:38 -0400

    Added sets of flashcards

--- BlazorPortfolio/BlazorPorfolio.Models/Flashcard.cs
+++ BlazorPortfolio/BlazorPorfolio.Models/Flashcard.cs
@@ -5,12 +5,22 @@ namespace BlazorPorfolio.Models
     public class Flashcard
     {
         public int Id { get; set; }
+
+
         [Required(ErrorMessage = "Front text is required")]
-
         public string FrontText { get; set; }
-        [Required(ErrorMessage = "Back text is required")]
 
+
+        [Required(ErrorMessage = "Back text is required")]
         public string BackText { get; set; }
+
         public int UserID { get; set; }
+
     }
+    public class FlashcardSet
+    {
+        public string SetName { get; set; }
+        public List<Flashcard> Flashcards { get; set; } = new List<Flashcard>();
+    }
+
 }
